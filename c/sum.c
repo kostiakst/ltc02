8219e4f1fece9fa5cdbbbcce47c2687b0ed7209f
U-glarus\varnenchik <kostiaka@protonmail.com>
2019-03-16 22:33:13 +0300

    code containing function for sum of two ints and printing the result

--- c/sum.c
+++ c/sum.c
@@ -1,13 +1,11 @@
 #include "stdio.h"
 
+int sum(int num1, int num2) {
+    int res = num1 + num2;
+    return res;
+}
+
 int main() {
-    
-    int sum(int num1, int num2) {
-	int res = num1 + num2;
-        return res;
-    }
-
     int result = sum(1, 2);
-
     printf("1 + 2 = %d", result);
 }
---
+++ js/sum.js
@@ -0,0 +1,8 @@
+function sum(num1, num2) {
+    let res = num1 + num2;
+    return res;
+}
+
+let result = sum(1, 2)
+
+console.log("1 + 2 = " + result)
