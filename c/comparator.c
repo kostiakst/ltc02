6b15cf144058a3035cc15d83ef740d873ea9b103
U-glarus\varnenchik <kostiaka@protonmail.com>
2019-04-12 00:19:26 +0300

    qsort with comparator

--- c/comparator.c
+++ c/comparator.c
@@ -6,21 +6,21 @@ int reverse_sorter(const void *first_arg, const void *second_arg) {
     int second = *(int*) second_arg;
 	
     if (first < second) { return 1; }
-	if (first > second) { return -1; }
-	if (first == second) { return 0; }
+    if (first > second) { return -1; }
+    if (first == second) { return 0; }
 
 }
 
 int main() {
-	int array[10] = {3, 5, 1, 7, 2, 7, 6, 0, 8, 4};
+    int array[10] = {3, 5, 1, 7, 2, 7, 6, 0, 8, 4};
 
-	int len = sizeof(array) / sizeof(array[0]);
+    int len = sizeof(array) / sizeof(array[0]);
 
-	qsort(array, len, sizeof(int), reverse_sorter);
+    qsort(array, len, sizeof(int), reverse_sorter);
 
-	int loop;
-	printf("sorted array: ");
-	for(loop = 0; loop < 10; loop++) {
-		printf("%d ", array[loop]);
-	}
+    int loop;
+    printf("sorted array: ");
+    for(loop = 0; loop < 10; loop++) {
+        printf("%d ", array[loop]);
+    }
 }
